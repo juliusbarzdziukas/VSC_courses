c0f5b6d659234eaaeec151d9f597a6b2d9e40985
Julius Barzdziukas <julius.barzdziukas@gmail.com>
2022-01-10 21:30:55 +0200

    ND-Kepykla

---
+++ Kepykla/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kepykla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            kepykla();
+            Console.ReadKey();
+        }
+        static void kepykla()
+        {
+            Console.Write("Darbuotojo kepiniai per valanda: ");
+            double kepalai_per_valanda = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Darbuotojai: ");
+            int darbuotojai = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Kepalo savikaina: ");
+            double savikaina = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Kepalo kaina: ");
+            double kaina = Convert.ToDouble(Console.ReadLine());
+            double kepalai = kepalai_per_valanda * 8.0 * darbuotojai;
+            Console.WriteLine($"Kepalai per diena: {kepalai}");
+            Console.WriteLine($"Kepalu savikaina: {kepalai * savikaina}");
+            Console.WriteLine($"Kepalu pajamos: {kepalai * kaina}");
+            Console.WriteLine($"Kepalu pelnas: {kepalai * (kaina - savikaina)}");
+        }
+    }
+}
