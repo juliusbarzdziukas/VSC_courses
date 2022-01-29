491a268e0b68dc958e1267a00ea312be645dedb4
Julius Barzdziukas <julius.barzdziukas@gmail.com>
2022-01-29 17:50:36 +0200

    Ciklai ND2, Speliojimo zaidimas

---
+++ Speliojimas/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Speliojimas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Zaidimas();
+            Console.ReadKey();
+        }
+        public void Zaidimas()
+        {
+            Random rnd = new Random();
+            int n = rnd.Next(1, 10);
+            int n1;
+            do
+            {
+                n1 = NuskaitytiInt("Atspekite skaiciu nuo 1 iki 10");
+                if (n1 > n) Console.WriteLine("Mazesnis");
+                else if (n1 < n) Console.WriteLine("Didesnis");
+            } while (n != n1);
+            Console.WriteLine("ATSPETA!");
+        }
+        public string NuskaitytiString(string S)
+        {
+            Console.Write($"{S}: ");
+            return Console.ReadLine();
+        }
+
+        public int NuskaitytiInt(string S)
+        {
+            return Convert.ToInt32(NuskaitytiString(S));
+        }
+    }
+}
